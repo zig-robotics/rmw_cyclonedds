82f17c016338c3d85c9999ac4c774e88a191dd6a
zig-robotics <141579568+zig-robotics@users.noreply.github.com>
2024-09-28 22:33:32 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Zig package for rmw cyclonedds
+
+This provides a zig package for the ros2 rmw_cyclonedds project.
+This currently targets zig 0.13 and ROS Jazzy.
+
+https://github.com/ros2/rmw_cyclonedds/tree/jazzy
---
+++ build.zig
@@ -0,0 +1,92 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+
+    const optimize = b.standardOptimizeOption(.{});
+
+    const linkage = b.option(std.builtin.LinkMode, "linkage", "Specify static or dynamic linkage") orelse .dynamic;
+    const upstream = b.dependency("rmw_cyclonedds", .{});
+    var lib = std.Build.Step.Compile.create(b, .{
+        .root_module = .{
+            .target = target,
+            .optimize = optimize,
+        },
+        .name = "rmw_cyclonedds_cpp",
+        .kind = .lib,
+        .linkage = linkage,
+    });
+
+    lib.linkLibCpp();
+
+    lib.addIncludePath(b.dependency("ros2_tracing", .{}).namedWriteFiles("tracetools").getDirectory());
+
+    const rcutils_dep = b.dependency("rcutils", .{
+        .target = target,
+        .optimize = optimize,
+        .linkage = linkage,
+    });
+    lib.linkLibrary(rcutils_dep.artifact("rcutils"));
+
+    const cyclonedds_dep = b.dependency("cyclonedds", .{
+        .target = target,
+        .optimize = optimize,
+        .linkage = linkage,
+    });
+    lib.linkLibrary(cyclonedds_dep.artifact("cyclonedds"));
+
+    const rcpputils_dep = b.dependency("rcpputils", .{
+        .target = target,
+        .optimize = optimize,
+        .linkage = linkage,
+    });
+    lib.linkLibrary(rcpputils_dep.artifact("rcpputils"));
+
+    const rmw_dep = b.dependency("rmw", .{
+        .target = target,
+        .optimize = optimize,
+        .linkage = linkage,
+    });
+    lib.linkLibrary(rmw_dep.artifact("rmw"));
+
+    const rmw_dds_common_dep = b.dependency("rmw_dds_common", .{
+        .target = target,
+        .optimize = optimize,
+        .linkage = linkage,
+    });
+    lib.linkLibrary(rmw_dds_common_dep.artifact("rmw_dds_common"));
+    lib.addIncludePath(rmw_dds_common_dep.namedWriteFiles("rmw_dds_common__rosidl_generator_cpp").getDirectory());
+
+    const rosidl_dep = b.dependency("rosidl", .{
+        .target = target,
+        .optimize = optimize,
+        .linkage = linkage,
+    });
+    lib.linkLibrary(rosidl_dep.artifact("rosidl_runtime_c"));
+    lib.linkLibrary(rosidl_dep.artifact("rosidl_typesupport_introspection_c"));
+    lib.linkLibrary(rosidl_dep.artifact("rosidl_typesupport_introspection_cpp"));
+    lib.linkLibrary(rosidl_dep.artifact("rosidl_dynamic_typesupport"));
+    lib.addIncludePath(rosidl_dep.namedWriteFiles("rosidl_typesupport_interface").getDirectory());
+    lib.addIncludePath(rosidl_dep.namedWriteFiles("rosidl_runtime_cpp").getDirectory());
+
+    lib.addIncludePath(upstream.path("rmw_cyclonedds/rmw_cyclonedds_cpp/src"));
+    lib.addCSourceFiles(.{
+        .root = upstream.path("rmw_cyclonedds_cpp/"),
+        .files = &.{
+            "src/rmw_get_network_flow_endpoints.cpp",
+            "src/rmw_node.cpp",
+            "src/serdata.cpp",
+            "src/serdes.cpp",
+            "src/u16string.cpp",
+            "src/exception.cpp",
+            "src/demangle.cpp",
+            "src/deserialization_exception.cpp",
+            "src/Serialization.cpp",
+            "src/TypeSupport2.cpp",
+            "src/TypeSupport.cpp",
+        },
+        .flags = &.{ "-Wno-deprecated-declarations", "--std=c++17" },
+    });
+
+    b.installArtifact(lib);
+}
